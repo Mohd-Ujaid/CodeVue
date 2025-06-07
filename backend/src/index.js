beb9003e6ceec0eaa344b97e63f4ab6eaa23e387
Mohd <mdujjaid0786@gmail.com>
2025-06-07 17:17:38 +0530

    fix: add missing origin URL for CORS configuration

--- backend/src/index.js
+++ backend/src/index.js
@@ -23,6 +23,7 @@ app.use(
       "http://localhost:5173",
       "http://localhost:5174",
       "http://localhost:5175",
+      "code-vue-git-main-mohd-ujaids-projects.vercel.app/"
     ],
     credentials: true,
   })
