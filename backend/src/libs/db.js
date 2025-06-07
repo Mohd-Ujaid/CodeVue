a8765ed2d0c8acc2a691dbf1d90c8ba747c129e6
Mohd <mdujjaid0786@gmail.com>
2025-06-07 15:47:36 +0530

    Remove generated Prisma schema and WASM files; update database connection to use Prisma Client directly from '@prisma/client'.

--- backend/src/libs/db.js
+++ backend/src/libs/db.js
@@ -10,7 +10,8 @@
 
 import { neon } from '@neondatabase/serverless';
 import { PrismaNeonHTTP } from '@prisma/adapter-neon';
-import { PrismaClient } from '../generated/prisma/index.js';
+// import { PrismaClient } from '../generated/prisma/index.js';
+import { PrismaClient } from '@prisma/client';
 
 const sql = neon(process.env.DATABASE_URL);
 const adapter = new PrismaNeonHTTP(sql);
